50e87eb88b984133cbc7959f9bc717b5a112fabb
akpatel21 <43002673+akpatel21@users.noreply.github.com>
2018-10-01 08:32:47 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,23 @@
+
+public class OperationsPractice 
+{
+
+	public static void main(String[] args) 
+	{
+		int number; //Data type then variable name
+		number = 20;
+		System.out.println("Number: " + number);
+		number = number + 5; //can be number += 5
+		System.out.println("Add 5: " + number);
+		number /= 2; //can be number = number / 2
+		System.out.println("Divide by 2: " + number);
+		number *= 4;
+		System.out.println("Multiply by 4: " + number);
+		number = number - 6;
+		System.out.println("Subtract 6: " + number);
+		number = number % 2;
+		System.out.println("Modularly divide by 2: " + number);
+
+	}
+
+}
